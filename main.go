c3670a13297cd92729dad1d79d1d91f21aa23250
Divyshekhar Sinha <divyshekhar2204@gmail.com>
2025-05-22 11:35:47 +0530

    add: cors

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/Divyshekhar/portfolio-backend/controllers"
 	"github.com/Divyshekhar/portfolio-backend/initializers"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -14,6 +15,12 @@ func init() {
 
 func main() {
 	router := gin.Default()
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"http://localhost:5173", "https://divyshekhar.vercel.app"},
+		AllowMethods: []string{"POST", "GET", "OPTIONS"},
+		AllowHeaders: []string{"Origin", "Content-Type", "Accept"},
+	}))
 
 	router.POST("/contact", controllers.MailController)
 	port := os.Getenv("PORT")
