348880e5ab52af1dcc8fe3bffe4fa58a8b65ca2c
Divyshekhar Sinha <divyshekhar2204@gmail.com>
2025-05-22 11:11:10 +0530

    fix: environment change

--- initializers/loadEnv.go
+++ initializers/loadEnv.go
@@ -2,13 +2,16 @@ package initializers
 
 import (
 	"log"
+	"os"
 
 	"github.com/joho/godotenv"
 )
 
 func LoadEnvVariables() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading environment variables")
+	if os.Getenv("RENDER") == "" {
+		err := godotenv.Load()
+		if err != nil {
+			log.Fatal("Error loading environment variables")
+		}
 	}
 }
